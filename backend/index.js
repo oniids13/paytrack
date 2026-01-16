9fa8aefe663f5f955e303eb630f1f031cf350644
oniids <jdino.abaya@gmail.com>
2026-01-16 15:08:44 +0800

    setup folders, install packages, prismaORM

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import session from "express-session";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
